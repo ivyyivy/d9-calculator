797b405ed618fa9921f401d68fd2d677e9aec1f9
ivy <dllmwl08@gmail.com>
2023-05-03 22:55:59 -0400

    fixed AC button

--- calculator.js
+++ calculator.js
@@ -1,99 +1,80 @@
-// 获取相关的DOM元素
-const display = document.getElementById("display");
-const keys = document.getElementById("grid-container");
+const display = document.getElementById('display');
+let result = '';
+let operator = '';
+let previousValue = '';
+let resetNext = false;
 
-// 记录用户输入的数字和运算符
-let currentInput = "";
-let currentOperator = null;
-let lastInput = "";
-
-// 用于清空当前输入的函数
-function clearInput() {
-    currentInput = "";
-    display.innerText = "0";
-}
-
-// 处理用户按下数字键的函数
-function handleNumberInput(number) {
-    currentInput += number;
-    display.innerText = currentInput;
-}
-
-// 处理用户按下运算符的函数
-function handleOperatorInput(operator) {
-    // 如果当前已经有一个运算符，则先计算上一次的结果
-    if (currentOperator) {
-        calculateResult();
+// 处理数字按钮点击事件
+function handleNumberClick(number) {
+    if (resetNext) {
+        display.innerText = '';
+        resetNext = false;
     }
-
-    // 记录当前的运算符和输入的数字
-    currentOperator = operator;
-    lastInput = currentInput;
-    currentInput = "";
+    if (display.innerText === '0' && number !== '.') {
+        display.innerText = '';
+    }
+    display.innerText += number;
 }
 
-// 处理用户按下等号键的函数
-function handleEqualInput() {
-    calculateResult();
+// 处理操作符点击事件
+function handleOperatorClick(op) {
+    previousValue = display.innerText;
+    operator = op;
+    resetNext = true;
 }
 
-// 计算结果的函数
-function calculateResult() {
-    let result;
-    const currentNumber = parseFloat(currentInput);
-    const lastNumber = parseFloat(lastInput);
+// 处理等号点击事件
+function handleEqualClick() {
+    const currentValue = display.innerText;
+    let calculation;
 
-    // 根据当前的运算符计算结果
-    switch (currentOperator) {
-        case "+":
-            result = lastNumber + currentNumber;
+    switch (operator) {
+        case '+':
+            calculation = Number(previousValue) + Number(currentValue);
             break;
-        case "-":
-            result = lastNumber - currentNumber;
+        case '-':
+            calculation = Number(previousValue) - Number(currentValue);
             break;
-        case "*":
-            result = lastNumber * currentNumber;
+        case '*':
+            calculation = Number(previousValue) * Number(currentValue);
             break;
-        case "/":
-            result = lastNumber / currentNumber;
+        case '/':
+            calculation = Number(previousValue) / Number(currentValue);
             break;
         default:
             return;
     }
 
-    // 将结果显示在display框中，并更新相关变量的值
+    result = calculation;
     display.innerText = result;
-    currentInput = result.toString();
-    currentOperator = null;
+    resetNext = true;
 }
 
-// 监听按键事件
-keys.addEventListener("click", function (event) {
-    const target = event.target;
+// 处理清零操作
+function handleResetClick() {
+    result = '';
+    operator = '';
+    previousValue = '';
+    display.innerText = '0';
+    resetNext = false;
+}
 
-    if (!target.matches("button")) {
-        return;
+// 处理按钮点击事件
+function handleClick(event) {
+    const keyValue = event.target.value;
+    if (!isNaN(keyValue)) {
+        handleNumberClick(keyValue);
+    } else if (keyValue === '.' && !display.innerText.includes('.')) {
+        handleNumberClick(keyValue);
+    } else if (keyValue === '+' || keyValue === '-' || keyValue === '*' || keyValue === '/') {
+        handleOperatorClick(keyValue);
+    } else if (keyValue === '=') {
+        handleEqualClick();
+    } else if (keyValue === 'AC') {
+        handleResetClick();
     }
+}
 
-    const value = target.value;
-
-    // 根据按键类型分别进行处理
-    switch (value) {
-        case "C":
-            clearInput();
-            break;
-        case "+":
-        case "-":
-        case "*":
-        case "/":
-            handleOperatorInput(value);
-            break;
-        case "=":
-            handleEqualInput();
-            break;
-        default:
-            handleNumberInput(value);
-            break;
-    }
-});
-
+// 添加按钮点击事件监听器
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', handleClick));
